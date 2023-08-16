3e7a0271c5ff0dc923440c7a56b73f94262069dd
Yannick <y.gorissen@student.fontys.nl>
2023-08-17 00:30:49 +0200

    message handling for data command

--- src/events/message.ts
+++ src/events/message.ts
@@ -1,7 +1,50 @@
-import { event, Events } from "../utils/index";
+import { event as messageEvent, Events } from "../utils/index";
+import pokeAPI from "../API/pokeAPI";
+import { Embed, EmbedBuilder } from "discord.js";
 
-export default event(Events.MessageCreate, ({ log }, msg) => {
-  if (msg.content.startsWith("!data")) {
-    msg.reply("Please enter something to search for.");
+export default messageEvent(Events.MessageCreate, ({ log }, msg) => {
+  if (!msg.content.startsWith("!data")) {
+    return;
   }
+
+  const args: string[] = msg.content.slice("!data".length).trim().split(" "); //removes prefix and organizes args
+
+  //handle less than 1 arg
+  if (args.length === undefined || args.length === 0) {
+    msg.reply(
+      "Please specify something to look for.\nUsage: `!data <pokemon name>`"
+    );
+    return;
+  }
+
+  if (args.length > 1) {
+    msg.reply("Please provide `1` argument to search for.");
+  }
+
+  const pokemonNameArg: string = args[1];
+
+  //call api to retrieve data
+  pokeAPI.getPokemon(pokemonNameArg).then((response) => {
+    const embed: EmbedBuilder = new EmbedBuilder()
+    .setTitle(response.name)
+    .setThumbnail(response.sprite)
+    .addFields(
+      {name: "Abilities", value: response.abilities.join(", ")},
+      {name: "Stats", value:
+      `**HP** ${response.stats.find(stat => stat.name === 'hp')?.value}\n` +
+      `**Atk** ${response.stats.find(stat => stat.name === 'attack')?.value}\n` +
+      `**Def** ${response.stats.find(stat => stat.name === 'defense')?.value}\n` +
+      `**SpA** ${response.stats.find(stat => stat.name === 'special-attack')?.value}\n` +
+      `**SpD** ${response.stats.find(stat => stat.name === 'special-defense')?.value}\n` +
+      `**Spe** ${response.stats.find(stat => stat.name === 'speed')?.value}`}
+    )
+
+    msg.reply({embeds: [embed]});
+  })
+  .catch(err => {
+    msg.reply("[Error] error fetching pokémon data");
+    log("[Error]: " + err.message);
+  })
+
+  
 });
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,5 @@
 import { event, Events } from '../utils/index'
 
 export default event(Events.ClientReady, ({log}, client) => { 
-    return log(`Logged in as ${client.user.username}.`);
+    return log(`\nLogged in as ${client.user.username}.`);
  } )
